dccdd3f8d0ebdc3e4c7e271d8a2699265a38a8de
babu-aravindh2002 <babuaravindh1@gmail.com>
2025-03-06 14:01:42 +0530

    changed the directory into frontend

--- backend/models/Country.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const countrySchema = new mongoose.Schema({
-  countryName: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  order: { type: Number },
-});
-
-module.exports = mongoose.model("Country", countrySchema);
--- backend/models/State.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const stateSchema = new mongoose.Schema({
-  stateName: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  order: { type: Number },
-});
-
-module.exports = mongoose.model("State", stateSchema);
